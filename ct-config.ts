e72dd93261ee4245169b0b14f526e69fb357fae9
Vlad <Vlad>
2025-05-04 12:52:21 +0200

    feat: add ct-config.ts file, env_example.txt file

---
+++ ct-config.ts
@@ -0,0 +1,9 @@
+require('dotenv').config()
+
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
+const project_key = process.env.PROJECT_KEY;
+const scope = process.env.SCOPE;
+const auth_url = process.env.AUTH_URL;
+const api_url = process.env.API_URL;
+  

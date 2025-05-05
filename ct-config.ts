fe7bd7ffc012b0152c27f2678e407c6f24de8059
Vlad <Vlad>
2025-05-05 15:50:10 +0200

    feat: pull updates from sprint-1/setup branch and keep single error type in ct-config.ts

--- ct-config.ts
+++ ct-config.ts
@@ -1,4 +1,6 @@
-require('dotenv').config()
+import { config } from 'dotenv';
+
+config();
 
 const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
@@ -6,4 +8,3 @@ const project_key = process.env.PROJECT_KEY;
 const scope = process.env.SCOPE;
 const auth_url = process.env.AUTH_URL;
 const api_url = process.env.API_URL;
-  

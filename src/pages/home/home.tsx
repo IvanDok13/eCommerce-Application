02be322f026e052347c3b9fbcc91cb1e15b2c7a8
Ivan Klochkov <vany-nov13@yandex.ru>
2025-05-11 15:00:56 -1000

    feat: add home page

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,9 @@
+import styles from './home.module.css';
+
+export function Home() {
+  return (
+    <div className={styles.home}>
+      <h1 className={styles.title}>Welcome to the Home Page</h1>
+    </div>
+  );
+}

b8d6c5a02cea887a375aab3f05fc44e2396bec50
Ivan Klochkov <vany-nov13@yandex.ru>
2025-05-11 19:26:54 -1000

    feat: update home whith header

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,9 +1,11 @@
+import { Header } from '@components/header/header';
 import styles from './home.module.css';
 
 export function Home() {
   return (
     <div className={styles.home}>
       <h1 className={styles.title}>Welcome to the Home Page</h1>
+      <Header />
     </div>
   );
 }

f3a5f49724cb11910ed8763bfa6b1f7d1ff25e08
Yuriy Biakov <gbyackov@gmail.com>
2025-05-16 18:29:12 +0300

    feat: add email input validation

--- src/components/registration-form/registration-form.tsx
+++ src/components/registration-form/registration-form.tsx
@@ -2,7 +2,7 @@ import { useForm } from 'react-hook-form';
 import styles from './registration-form.module.css';
 import type { FC } from 'react';
 import type { FormData } from './registration-form.types';
-
+import { validationRules } from '@utils/validation-rules';
 
 export const RegistrationForm: FC = () => {
   const {
@@ -11,13 +11,16 @@ export const RegistrationForm: FC = () => {
     formState: { errors }, //TO DO: implement error handling
   } = useForm<FormData>();
 
-  const onSubmit = async (data: FormData) : Promise<void> => {
+  const onSubmit = async (data: FormData): Promise<void> => {
     console.log('Form data:', data); //TO DO: implement form submission
   };
 
   return (
-    <form className={styles.registrationForm} onSubmit= {(event) => void handleSubmit(onSubmit)(event)}>
-      <input className={styles.registrationInput} {...register('email')} placeholder="Email" />
+    <form className={styles.registrationForm} onSubmit={event => void handleSubmit(onSubmit)(event)}>
+      <div className={styles.registrationInputContainer}>
+        <input className={styles.registrationInput} {...register('email', validationRules.email)} placeholder="Email" />
+        {errors.email && <p className={styles.errorMessage}>{errors.email.message}</p>}
+      </div>
       <input className={styles.registrationInput} {...register('password')} placeholder="Password" />
       <input className={styles.registrationInput} {...register('firstName')} placeholder="First Name" />
       <input className={styles.registrationInput} {...register('lastName')} placeholder="Last Name" />
@@ -27,7 +30,9 @@ export const RegistrationForm: FC = () => {
       <input className={styles.registrationInput} {...register('postalCode')} placeholder="Postal Code" />
       <input className={styles.registrationInput} {...register('country')} placeholder="Country" />
 
-      <button className={styles.submitButton} type="submit">Register</button>
+      <button className={styles.submitButton} type="submit">
+        Register
+      </button>
     </form>
   );
 };

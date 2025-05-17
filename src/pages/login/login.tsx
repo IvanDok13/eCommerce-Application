9bee84269cf8a772cdb25c38e16090b9707e6adf
Vlad <Vlad>
2025-05-17 14:30:01 +0200

    feat: create form page with all styles, components, types

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,18 +1,14 @@
 import { Header } from '@components/header/header';
+import { LoginForm } from '@components/login-form/login-form';
 import type { FC } from 'react';
 import styles from './login.module.css';
 
-interface LoginForm {
-    email: string;
-    password: string;
-  }
-
 export const Login: FC = () => {
-    return (
-        <div className={styles.home}>
-        <h1 className={styles.title}>Welcome to the Login Page</h1>
-        <Header />
-      </div>
-    )
-}
-
+  return (
+    <div className={styles.home}>
+      <Header />
+      <h1 className={styles.pageTitle}>Login page</h1>
+      <LoginForm />
+    </div>
+  );
+};

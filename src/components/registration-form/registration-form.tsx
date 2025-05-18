77fef492a726342640f16179930221c08d8f52f4
Yuriy Biakov <gbyackov@gmail.com>
2025-05-18 19:30:46 +0300

    feat: implement customer registration form submission and mapping

--- src/components/registration-form/registration-form.tsx
+++ src/components/registration-form/registration-form.tsx
@@ -3,6 +3,8 @@ import styles from './registration-form.module.css';
 import type { FC } from 'react';
 import type { FormData } from './registration-form.types';
 import { validationRules } from '@utils/validation-rules';
+import type { CustomerDraft } from '@commercetools/platform-sdk';
+import { registerCustomer } from 'src/api/customers-api';
 
 export const RegistrationForm: FC = () => {
   const {
@@ -14,7 +16,16 @@ export const RegistrationForm: FC = () => {
   });
 
   const onSubmit = async (data: FormData): Promise<void> => {
-    console.log('Form data:', data); //TO DO: implement form submission
+    try {
+      const customerDraft = mapFormDataToCustomerDraft(data);
+      const result = await registerCustomer(customerDraft);
+      console.log('Registered:', result.body.customer);
+      // TODO: Redirect or show success message
+    } catch (error : unknown) {
+       if (error instanceof Error) 
+      console.error('Registration failed:', error);
+      // TODO: show error message in UI
+    }
   };
 
   return (
@@ -24,23 +35,44 @@ export const RegistrationForm: FC = () => {
         {errors.email && <p className={styles.errorMessage}>{errors.email.message}</p>}
       </div>
       <div className={styles.registrationInputContainer}>
-        <input className={styles.registrationInput} {...register('password', validationRules.password)} placeholder="Password" />
+        <input
+          className={styles.registrationInput}
+          {...register('password', validationRules.password)}
+          placeholder="Password"
+        />
         {errors.password && <p className={styles.errorMessage}>{errors.password.message}</p>}
       </div>
       <div className={styles.registrationInputContainer}>
-        <input className={styles.registrationInput} {...register('firstName', validationRules.name)} placeholder="First Name" />
+        <input
+          className={styles.registrationInput}
+          {...register('firstName', validationRules.name)}
+          placeholder="First Name"
+        />
         {errors.firstName && <p className={styles.errorMessage}>{errors.firstName.message}</p>}
       </div>
       <div className={styles.registrationInputContainer}>
-        <input className={styles.registrationInput} {...register('lastName', validationRules.name)} placeholder="Last Name" />
+        <input
+          className={styles.registrationInput}
+          {...register('lastName', validationRules.name)}
+          placeholder="Last Name"
+        />
         {errors.lastName && <p className={styles.errorMessage}>{errors.lastName.message}</p>}
       </div>
       <div className={styles.registrationInputContainer}>
-        <input className={styles.registrationInput} type="date" {...register('dateOfBirth', validationRules.dateOfBirth)} placeholder="Date of Birth" />
+        <input
+          className={styles.registrationInput}
+          type="date"
+          {...register('dateOfBirth', validationRules.dateOfBirth)}
+          placeholder="Date of Birth"
+        />
         {errors.dateOfBirth && <p className={styles.errorMessage}>{errors.dateOfBirth.message}</p>}
       </div>
       <div className={styles.registrationInputContainer}>
-        <input className={styles.registrationInput} {...register('street', validationRules.street)} placeholder="Street" />
+        <input
+          className={styles.registrationInput}
+          {...register('street', validationRules.street)}
+          placeholder="Street"
+        />
         {errors.street && <p className={styles.errorMessage}>{errors.street.message}</p>}
       </div>
       <div className={styles.registrationInputContainer}>
@@ -48,11 +80,19 @@ export const RegistrationForm: FC = () => {
         {errors.city && <p className={styles.errorMessage}>{errors.city.message}</p>}
       </div>
       <div className={styles.registrationInputContainer}>
-        <input className={styles.registrationInput} {...register('postalCode', validationRules.postalCode)} placeholder="Postal Code" />
+        <input
+          className={styles.registrationInput}
+          {...register('postalCode', validationRules.postalCode)}
+          placeholder="Postal Code"
+        />
         {errors.postalCode && <p className={styles.errorMessage}>{errors.postalCode.message}</p>}
       </div>
       <div className={styles.registrationInputContainer}>
-        <input className={styles.registrationInput} {...register('country', validationRules.country)} placeholder="Country" />
+        <input
+          className={styles.registrationInput}
+          {...register('country', validationRules.country)}
+          placeholder="Country"
+        />
         {errors.country && <p className={styles.errorMessage}>{errors.country.message}</p>}
       </div>
       <button className={styles.submitButton} type="submit">
@@ -61,3 +101,21 @@ export const RegistrationForm: FC = () => {
     </form>
   );
 };
+
+export const mapFormDataToCustomerDraft = (data: FormData): CustomerDraft => ({
+  email: data.email,
+  password: data.password,
+  firstName: data.firstName,
+  lastName: data.lastName,
+  dateOfBirth: data.dateOfBirth,
+  addresses: [
+    {
+      streetName: data.street,
+      city: data.city,
+      postalCode: data.postalCode,
+      country: data.country.toUpperCase(),
+    },
+  ],
+  defaultShippingAddress: 0,
+  defaultBillingAddress: 0,
+});

54a8c789c45c722452045f7a52dd813bce318fc3
Yuriy Biakov <gbyackov@gmail.com>
2025-05-19 20:28:05 +0300

    feat: restructure FormData to include shipping and billing addresses with optional fields

--- src/components/registration-form/registration-form.types.ts
+++ src/components/registration-form/registration-form.types.ts
@@ -1,12 +1,23 @@
-
 export type FormData = {
   email: string;
   password: string;
   firstName: string;
   lastName: string;
   dateOfBirth: string;
-  street: string;
-  city: string;
-  postalCode: string;
-  country: string;
+
+  shippingAddress: {
+    street: string;
+    city: string;
+    postalCode: string;
+    country: string;
+  };
+
+  billingAddress: {
+    street?: string;
+    city?: string;
+    postalCode?: string;
+    country?: string;
+  };
+
+  useSameAddress: boolean;
 };

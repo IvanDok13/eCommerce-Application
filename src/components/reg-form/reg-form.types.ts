a28ef840a3f18dd1cc6c729afedca968f2f6b21e
Ivan Klochkov <vany-nov13@yandex.ru>
2025-05-30 01:27:54 -1000

    fix: remove default settings for billing adress, rename file

---
+++ src/components/reg-form/reg-form.types.ts
@@ -0,0 +1,25 @@
+export type FormData = {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  dateOfBirth: string;
+
+  shippingAddress: {
+    street: string;
+    city: string;
+    postalCode: string;
+    country: string;
+  };
+
+  billingAddress: {
+    street: string;
+    city: string;
+    postalCode: string;
+    country: string;
+  };
+
+  useSameAddress: boolean;
+  defaultShippingAddress: boolean;
+  defaultBillingAddress: boolean;
+};

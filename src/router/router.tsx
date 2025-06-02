ff2ef7d65c6bd6fe3a86e96e9f659b41c4bd2298
Yuriy Biakov <gbyackov@gmail.com>
2025-06-02 23:06:40 +0300

    feat: add catalog route and component to the router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -5,6 +5,7 @@ import { Registration } from '@pages/registration/registration';
 import type { FC } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AppRoutes } from './routes';
+import { Catalog } from '@pages/catalog/catalog';
 
 export const AppRouter: FC = () => (
   <BrowserRouter>
@@ -12,6 +13,7 @@ export const AppRouter: FC = () => (
       <Route path={AppRoutes.HOME_ROUTE} element={<Home />} />
       <Route path={AppRoutes.LOGIN_ROUTE} element={<Login />} />
       <Route path={AppRoutes.REGISTRATION_ROUTE} element={<Registration />} />
+      <Route path={AppRoutes.CATALOG_ROUTE} element={<Catalog />} />
       <Route path={AppRoutes.EROR_ROUTE} element={<NotFoundPage />} />
     </Routes>
   </BrowserRouter>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,5 +2,6 @@ export enum AppRoutes {
   HOME_ROUTE = '/',
   LOGIN_ROUTE = '/login',
   REGISTRATION_ROUTE = '/registration',
+  CATALOG_ROUTE = '/catalog',
   EROR_ROUTE = '*',
 }

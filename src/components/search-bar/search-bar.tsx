16b85094ecc7accb251c7a337dcd75404ed155fb
Yuriy Biakov <gbyackov@gmail.com>
2025-06-04 01:36:22 +0300

    feat: update SearchBar component to include searchQuery and handleChange props

--- src/components/search-bar/search-bar.tsx
+++ src/components/search-bar/search-bar.tsx
@@ -1,14 +1,23 @@
-import { type FC } from 'react';
+import React, { type FC } from 'react';
 import styles from './search-bar.module.css';
+import { type SearchBarProps } from './search-bar.types';
 
-export const SearchBar: FC = () => {
+export const SearchBar: FC<SearchBarProps> = ({ searchQuery, setSearchQuery }) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleClear = (): void => {
+    setSearchQuery('');
+  };
+
   return (
     <div className={styles.searchBarContainer}>
       <input
         type="text"
         placeholder="Search for a tattoo..."
-        // value={searchQuery}
-        // onChange={handleChange}
+        value={searchQuery}
+        onChange={handleChange}
         className={styles.searchInput}
       />
       {<button className={styles.searchButton}>Search</button>}

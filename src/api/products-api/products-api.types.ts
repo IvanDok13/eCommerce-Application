43e7cef562b5aa4816cc4922ff70d501d7a9a306
Yuriy Biakov <gbyackov@gmail.com>
2025-06-04 18:11:33 +0300

    feat: add ProductRenderData interface for product details representation

---
+++ src/api/products-api/products-api.types.ts
@@ -0,0 +1,11 @@
+export interface ProductRenderData {
+  id: string;
+  name: string;
+  slug: string;
+  price: string;
+  imageUrls: string[];
+  artist: string;
+  size: string;
+  color: string;
+  description: string;
+}

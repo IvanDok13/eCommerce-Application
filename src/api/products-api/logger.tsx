e26ab12394fbf6c5a7dc154a155db8c32174d7af
Yuriy Biakov <gbyackov@gmail.com>
2025-06-04 18:39:46 +0300

    chore: update Logger component to use mapProductToRenderData for product logging

--- src/api/products-api/logger.tsx
+++ src/api/products-api/logger.tsx
@@ -1,23 +1,13 @@
 import React, { useEffect } from 'react';
-import { fetchProducts } from './products-api';
+import { fetchProducts, mapProductToRenderData } from './products-api';
 
 const Logger: React.FC = () => {
   useEffect(() => {
-    fetchProducts(6)
+    fetchProducts(25)
       .then(products => {
-        products.forEach(product => {
-          const id = product.id;
-          const name = product.masterData.current.name['en-US'];
-          const priceCents = product.masterData.current.masterVariant.prices?.[0]?.value.centAmount;
-          const price = priceCents ? priceCents / 100 : 'N/A';
-          const imageUrl = product.masterData.current.masterVariant.images?.[0]?.url;
+        const productsForRender = products.map(mapProductToRenderData);
 
-          console.log(`Product id: ${id}`);
-          console.log(`Name: ${name}`);
-          console.log(`Price: $${price}`);
-          console.log(`Image URL: ${imageUrl}`);
-          console.log('-----------------------------------');
-        });
+        console.log('Mapped Products:', productsForRender);
       })
       .catch(error => {
         console.error('Error fetching products:', error);

ab109052a460f334c7d36b364ddeea8024ea1ebf
Yuriy Biakov <gbyackov@gmail.com>
2025-06-05 16:08:31 +0300

    feat: add routing for DetailedProduct page

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -5,6 +5,7 @@ export enum AppRoutes {
   USER_PROFILE_ROUTE = '/profile',
   CATALOG_ROUTE = '/catalog',
   CATALOG_CATEGORY_ROUTE = '/catalog/:categorySlug',
+  PRODUCT_DETAIL_ROUTE = '/product/:productId',
   // TO DO: implement this - CATALOG_CATEGORY_ROUTE = '/catalog/*',
   ERROR_ROUTE = '*',
 }

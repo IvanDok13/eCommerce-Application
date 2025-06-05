ee658b34d26d602d8453e84dd7e4d2c87076755e
Yuriy Biakov <gbyackov@gmail.com>
2025-06-05 16:05:39 +0300

    refactor: update ProductCard component to include product ID in the link

--- src/components/product-card/product-card.tsx
+++ src/components/product-card/product-card.tsx
@@ -1,16 +1,19 @@
 import { type FC } from 'react';
+import { Link } from 'react-router-dom';
 import styles from './product-card.module.css';
 import { type ProductCardProps } from './product-card.types.ts';
 
-export const ProductCard: FC<ProductCardProps> = ({ name, price, artist, color, size, imageUrls }) => {
+export const ProductCard: FC<ProductCardProps> = ({ id, name, price, artist, color, size, imageUrls }) => {
   return (
-    <div className={styles.productCard}>
-      <img src={imageUrls[0]} alt={name} className={styles.productImage} />
-      <h4 className={styles.productName}>{name}</h4>
-      <p className={styles.productPrice}>${price / 100}</p>
-      <p className={styles.productDetails}>
-        {artist} | {color} | {size}
-      </p>
-    </div>
+    <Link to={`/product/${id}`} className={styles.productCardLink}>
+      <div className={styles.productCard}>
+        <img src={imageUrls[0]} alt={name} className={styles.productImage} />
+        <h4 className={styles.productName}>{name}</h4>
+        <p className={styles.productPrice}>${price / 100}</p>
+        <p className={styles.productDetails}>
+          {artist} | {color} | {size}
+        </p>
+      </div>
+    </Link>
   );
 };

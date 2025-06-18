1964118d2ae60c4925e73fe30e019e5246ee665d
Ivan Klochkov <vany-nov13@yandex.ru>
2025-06-18 01:55:14 -1000

    fix: solve merge conflicts

--- src/components/login-form/login-form.tsx
+++ src/components/login-form/login-form.tsx
@@ -1,131 +1,43 @@
-import { authRequestResponse } from '@api/auth-user';
-import { authError } from '@utils/auth-error';
-import { validationRules } from '@utils/validation-rules';
-import type { FC } from 'react';
-import { useContext, useEffect, useState } from 'react';
-import { type SubmitHandler, useForm } from 'react-hook-form';
-import { FaEye, FaEyeSlash } from 'react-icons/fa';
-import { Link, useNavigate } from 'react-router-dom';
-import { Tooltip } from 'react-tooltip';
-import { authContext } from 'src/context/auth-provider';
-import styles from './login-form.module.css';
-import type { LoginFormData } from './login-form.types';
+import type { Context, FC, ReactNode } from 'react';
+import { createContext, useState } from 'react';
 
-export const LoginForm: FC = () => {
-  const [showPassword, setShowPassword] = useState<boolean>(false);
+export type authContextTupe = {
+  login: string;
+  isLoginned: boolean;
+  customerId: string;
+  setLogin: (login: string) => void;
+  setIsLoginned: (isLoginned: boolean) => void;
+  setCustomerId: (customerId: string) => void;
+};
 
-  const navigate = useNavigate();
+const defaultContext: authContextTupe = {
+  login: '',
+  isLoginned: false,
+  customerId: '',
+  setLogin: () => {},
+  setIsLoginned: () => {},
+  setCustomerId: () => {},
+};
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    setError,
-  } = useForm<LoginFormData>({ mode: 'all' });
+export const authContext: Context<authContextTupe> = createContext(defaultContext);
 
-  const [apiError, setApiError] = useState<{
-    field: 'email' | 'password' | 'general';
-    message: string;
-  } | null>(null);
+export const AuthProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const [login, setLogin] = useState('');
 
-  const { isLoginned, setLogin, setIsLoginned, setCustomerId } = useContext(authContext);
+  const [customerId, setCustomerId] = useState(() => {
+    return localStorage.getItem('customerId') || '';
+  });
 
-  useEffect(() => {
-    if (isLoginned) {
-      navigate('/');
-    }
-  }, [isLoginned, navigate]);
+  const [anonymousId, setAnonymousId] = useState(() => {
+    return localStorage.getItem('anonymousId') || '';
+  });
 
-  const formSubmit: SubmitHandler<LoginFormData> = async data => {
-    try {
-      const response = await authRequestResponse(data.email, data.password);
-      const customer = response.body.customer;
-      setLogin(customer.email);
-      setIsLoginned(true);
-      setCustomerId(customer.id);
-      localStorage.setItem('customerId', response.body.customer.id);
-      navigate('/');
-    } catch (error) {
-      const authApiError = authError(error);
-      setApiError(authApiError);
-      switch (authApiError?.field) {
-        case 'email':
-          setError('email', { type: 'manual', message: authApiError.message });
-          break;
-        case 'password':
-          setError('password', { type: 'manual', message: authApiError.message });
-          break;
-        case 'general':
-          setError('email', { type: 'manual', message: authApiError.message });
-          setError('password', { type: 'manual', message: authApiError.message });
-          break;
-        default:
-          break;
-      }
-    }
-    console.log('Form submitted:', data);
-  };
-
+  const [isLoginned, setIsLoginned] = useState(() => {
+    return Boolean(localStorage.getItem('Token'));
+  });
   return (
-    <main className={styles.main}>
-      <form className={styles.loginForm} action="submit" onSubmit={event => void handleSubmit(formSubmit)(event)}>
-        <div className="formBlockContainer">
-          <div className={styles.registrationInputContainer}>
-            <label htmlFor="email" className={styles.label} form="email">
-              Email
-            </label>
-            <input
-              id="email"
-              type="text"
-              className={styles.input}
-              placeholder="email@gmail.com"
-              data-tooltip-id="tooltip-email"
-              data-tooltip-content={errors.email?.message || (apiError?.field === 'email' ? apiError.message : '')}
-              {...register('email', validationRules.email)}
-            />
-          </div>
-          <Tooltip
-            id="tooltip-email"
-            place="top"
-            variant="error"
-            isOpen={!!errors.email || apiError?.field === 'email'}
-          />
-
-          <div className={styles.registrationInputContainer}>
-            <label htmlFor="password" className={styles.label}>
-              Password
-            </label>
-            <input
-              id="password"
-              type={showPassword ? 'text' : 'password'}
-              className={styles.input}
-              placeholder="Abcde123"
-              data-tooltip-id="tooltip-password"
-              data-tooltip-content={
-                errors.password?.message || (apiError?.field === 'password' ? apiError.message : '')
-              }
-              {...register('password', validationRules.password)}
-            />
-
-            <button type="button" className="show-password" onClick={() => setShowPassword(!showPassword)}>
-              {showPassword ? <FaEyeSlash /> : <FaEye />}
-            </button>
-          </div>
-          <Tooltip
-            id="tooltip-password"
-            place="top"
-            variant="error"
-            isOpen={!!errors.password || apiError?.field === 'password'}
-          />
-        </div>
-
-        <button type="submit" className="registrationButton">
-          Login
-        </button>
-        <Link to="/registration" className={styles.signUpButton}>
-          Sign up
-        </Link>
-      </form>
-    </main>
+    <authContext.Provider value={{ login, isLoginned, customerId, setLogin, setIsLoginned, setCustomerId }}>
+      {children}
+    </authContext.Provider>
   );
 };
